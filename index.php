b94e4adecb45e9887e57d8aed0656dc27c894748
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2024-02-03 09:31:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Hello, World!</title>
 </head>
-<body>
+<body bgcolor = "red">
         <h1><?php echo 'Hello, World!'; ?></h1>
 </body>
 </html>
